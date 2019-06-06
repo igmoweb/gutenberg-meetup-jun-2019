31573cdad41d1660f5bd4950169743f81f423704
Ignacio Cruz <igmoweb@gmail.com>
2019-06-06 18:47:59 +0200

    Added deprecated block

--- src/blocks/call-to-action/index.js
+++ src/blocks/call-to-action/index.js
@@ -3,32 +3,34 @@ const { __ } = wp.i18n;
 const { RichText } = wp.editor;
 const { TextControl } = wp.components;
 
+const attributes = {
+	content: {
+		type: 'string',
+		default: '',
+		source: 'html',
+		selector: '.wp-block-meetup-call-to-action__content'
+	},
+	title: {
+		type: 'string',
+		default: '',
+		source: 'text',
+		selector: 'h2'
+	},
+	url: {
+		type: 'string',
+		default: '',
+		source: 'attribute',
+		attribute: 'href',
+		selector: 'a'
+	}
+};
+
 // Register the click to tweet block
 registerBlockType( 'meetup/call-to-action', {
 	title: __( 'Call To Action', 'meetup' ),
 	icon: 'megaphone',
 	category: 'meetup',
-	attributes: {
-		content: {
-			type: 'string',
-			default: '',
-			source: 'html',
-			selector: '.wp-block-meetup-call-to-action__content'
-		},
-		title: {
-			type: 'string',
-			default: '',
-			source: 'text',
-			selector: 'h3'
-		},
-		url: {
-			type: 'string',
-			default: '',
-			source: 'attribute',
-			attribute: 'href',
-			selector: 'a'
-		}
-	},
+	attributes,
 	edit: ( props ) => {
 		return <div className={ props.className }>
 			<RichText
@@ -36,7 +38,7 @@ registerBlockType( 'meetup/call-to-action', {
 				value={ props.attributes.title }
 				multiline={ false }
 				formattingControls={ [] }
-				tagName="h3"
+				tagName="h2"
 				onChange={ ( newTitle ) => { props.setAttributes( { title: newTitle } ) } }
 			/>
 			<div className={ `${props.className}__content`}>
@@ -63,7 +65,7 @@ registerBlockType( 'meetup/call-to-action', {
 	save: ( props ) => {
 		const className = 'wp-block-meetup-call-to-action';
 		return <div>
-			<h3>{ props.attributes.title }</h3>
+			<h2>{ props.attributes.title }</h2>
 			<div className={ `${className}__content` }>
 				<RichText.Content multiline={ true } value={ props.attributes.content } />
 			</div>
@@ -71,5 +73,30 @@ registerBlockType( 'meetup/call-to-action', {
 				Apply Now
 			</a>
 		</div>;
-	}
+	},
+	deprecated: [
+		{
+			attributes: {
+				...attributes,
+				title: {
+					type: 'string',
+					default: '',
+					source: 'text',
+					selector: 'h3'
+				},
+			},
+			save: ( props ) => {
+				const className = 'wp-block-meetup-call-to-action';
+				return <div>
+					<h3>{ props.attributes.title }</h3>
+					<div className={ `${className}__content` }>
+						<RichText.Content multiline={ true } value={ props.attributes.content } />
+					</div>
+					<a className={ `${className}__link`} href={ props.attributes.url }>
+						Apply Now
+					</a>
+				</div>;
+			},
+		}
+	]
 } );
